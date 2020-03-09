ad6e7e04cd7d0f8b6eab62d066c6f2ee4fe5bfbc
Benoit Lavenier <benoit.lavenier@e-is.pro>
2020-03-09 15:01:38 +0100

    [fix] Notification: Make sure notification has always and unique ID [fix] Notification: show issuer of page/ureser events

--- www/plugins/es/js/entities/notification.js
+++ www/plugins/es/js/entities/notification.js
@@ -15,14 +15,15 @@ function EsNotification(json, markAsReadCallback) {
   // Avoid undefined errors
   json = json || {};
 
+  that.id = json.id || ('' + Date.now()); // Keep id if exists, otherwise create it from timestamp
   that.type = json.type && json.type.toLowerCase();
   that.time = json.time;
   that.hash = json.hash;
   that.read = json.read_signature ? true : false;
 
   that.message = json.reference && messagePrefixes[json.reference.index] ?
-  messagePrefixes[json.reference.index] + json.code :
-  'EVENT.' + json.code;
+    messagePrefixes[json.reference.index] + json.code :
+    'EVENT.' + json.code;
   that.params = json.params;
 
   if (markAsReadCallback && (typeof markAsReadCallback === "function") ) {
@@ -47,12 +48,15 @@ function EsNotification(json, markAsReadCallback) {
   if (json.code.startsWith('MEMBER_')) {
     that.avatarIcon = 'ion-person';
     that.icon = 'ion-information-circled positive';
+    that.state = 'app.view_wallet';
+    that.medianTime = that.time;
   }
 
   // TX
   else if (json.code.startsWith('TX_')) {
     that.avatarIcon = 'ion-card';
     that.icon = (json.code === 'TX_SENT') ? 'ion-paper-airplane dark' : 'ion-archive balanced';
+    that.medianTime = that.time;
     pubkeys = json.params.length > 0 ? json.params[0] : null;
     if (pubkeys && pubkeys.indexOf(',') == -1) {
       that.pubkey = pubkeys;
@@ -66,7 +70,11 @@ function EsNotification(json, markAsReadCallback) {
     that.avatarIcon = (json.code === 'CERT_RECEIVED') ? 'ion-ribbon-b' : 'ion-ribbon-a';
     that.icon = (json.code === 'CERT_RECEIVED') ? 'ion-ribbon-b balanced' : 'ion-ribbon-a gray';
     that.pubkey = json.params.length > 0 ? json.params[0] : null;
-    that.state = (json.code === 'CERT_RECEIVED') ? 'app.wallet_cert.tab_received' : 'app.wallet_cert.tab_given';
+    that.medianTime = that.time;
+    that.state = 'app.wallet_cert';
+    that.stateParams = {
+      type: (json.code === 'CERT_RECEIVED') ? 'received' : 'given'
+    };
   }
 
   // Message
@@ -82,7 +90,17 @@ function EsNotification(json, markAsReadCallback) {
 
   // user profile record
   else if (json.reference && json.reference.index === 'user' && json.reference.type === 'profile') {
-    if (json.code.startsWith('STAR_')) {
+    that.pubkey = json.params.length > 0 ? json.params[0] : null;
+    that.state = 'app.wot_identity';
+    that.stateParams = {
+      pubkey: that.pubkey,
+      uid: json.params && json.params[3],
+    };
+    if (json.code.startsWith('LIKE_')) {
+      that.avatarIcon = 'ion-person';
+      that.icon = 'ion-ios-heart positive';
+    }
+    else if (json.code.startsWith('STAR_')) {
       that.avatarIcon = 'ion-person';
       that.icon = 'ion-star gray';
     }
@@ -90,18 +108,34 @@ function EsNotification(json, markAsReadCallback) {
       that.avatarIcon = 'ion-person';
       that.icon = 'ion-ios-people gray';
     }
-    else if (json.code.startsWith('LIKE_')) {
+    else if (json.code.startsWith('ABUSE_')) {
       that.avatarIcon = 'ion-person';
-      that.icon = 'ion-ios-heart positive';
+      that.icon = 'ion-android-warning assertive';
+    }
+    if (json.code.startsWith('MODERATION_')) {
+      that.state = 'app.wot_identity';
+      that.stateParams = {
+        pubkey: json.reference.id,
+        uid: json.params && json.params[3],
+      };
+      that.avatarIcon = 'ion-alert-circled';
+      that.icon = 'ion-alert-circled energized';
+      // If deletion has been asked, change the message
+      var level = json.params && json.params[4] || 0;
+      if (json.code === 'MODERATION_RECEIVED' && level == 5) {
+        that.message = 'EVENT.USER.DELETION_RECEIVED';
+        that.icon = 'ion-trash-a assertive';
+      }
     }
     else {
       that.icon = 'ion-person dark';
+      that.state = 'app.view_wallet';
     }
-    that.state = 'app.view_wallet';
   }
 
   // page record
   else if (json.reference && json.reference.index === 'page') {
+    that.pubkey = json.params.length > 0 ? json.params[0] : null;
     that.avatarIcon = 'ion-social-buffer';
     if (json.reference.anchor) {
       that.icon = 'ion-ios-chatbubble-outline dark';
@@ -117,20 +151,44 @@ function EsNotification(json, markAsReadCallback) {
       that.state = 'app.view_page';
       that.stateParams = {
         id: json.reference.id,
-        title: json.params[1]};
+        title: json.params[1]
+      };
     }
-    if (json.code.startsWith('MODERATION_')) {
-      that.avatarIcon = 'ion-alert-circled';
-      that.icon = 'ion-alert-circled energized';
-    }
-    else if (json.code.startsWith('ABUSE_')) {
-      that.icon = 'ion-alert-circled energized';
-    }
-    else if (json.code.startsWith('LIKE_')) {
+
+    if (json.code.startsWith('LIKE_')) {
       that.icon = 'ion-ios-heart positive';
+      that.state = 'app.wot_identity';
+      that.stateParams = {
+        pubkey: that.pubkey,
+        uid: json.params && json.params[3],
+      };
     }
     else if (json.code.startsWith('FOLLOW_')) {
       that.avatarIcon = 'ion-person';
+      that.state = 'app.wot_identity';
+      that.stateParams = {
+        pubkey: that.pubkey,
+        uid: json.params && json.params[3],
+      };
+    }
+    else if (json.code.startsWith('ABUSE_')) {
+      that.icon = 'ion-alert-circled energized';
+      that.state = 'app.wot_identity';
+      that.stateParams = {
+        pubkey: that.pubkey,
+        uid: json.params && json.params[3],
+      };
+    }
+    else if (json.code.startsWith('MODERATION_')) {
+      that.avatarIcon = 'ion-alert-circled';
+      that.icon = 'ion-alert-circled energized';
+
+      // If deletion has been asked, change the message
+      var level = json.params && json.params[4] || 0;
+      if (json.code === 'MODERATION_RECEIVED' && level == 5) {
+        that.message = 'EVENT.PAGE.DELETION_RECEIVED';
+        that.icon = 'ion-trash-a assertive';
+      }
     }
   }
 
@@ -155,19 +213,27 @@ function EsNotification(json, markAsReadCallback) {
         id: json.reference.id,
         title: json.params[2]};
     }
-    if (json.code.startsWith('MODERATION_')) {
-      that.avatarIcon = 'ion-alert-circled';
-      that.icon = 'ion-alert-circled energized';
-    }
-    else if (json.code.startsWith('ABUSE_')) {
-      that.icon = 'ion-alert-circled energized';
-    }
-    else if (json.code.startsWith('LIKE_')) {
+
+    if (json.code.startsWith('LIKE_')) {
       that.icon = 'ion-ios-heart positive';
     }
     else if (json.code.startsWith('FOLLOW_')) {
       that.avatarIcon = 'ion-person';
     }
+    else if (json.code.startsWith('ABUSE_')) {
+      that.icon = 'ion-alert-circled energized';
+    }
+    else if (json.code.startsWith('MODERATION_')) {
+      that.avatarIcon = 'ion-alert-circled';
+      that.icon = 'ion-alert-circled energized';
+
+      // If deletion has been asked, change the message
+      if (json.code === 'MODERATION_RECEIVED' && level == 5) {
+        that.message = 'EVENT.MARKET.DELETION_RECEIVED';
+        that.icon = 'ion-trash-a assertive';
+      }
+    }
+
   }
 
   // info message
