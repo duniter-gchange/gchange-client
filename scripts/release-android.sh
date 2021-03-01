505d5154e6a12a6c3cc9689a388dc7f0b1675470
Benoit Lavenier <benoit.lavenier@e-is.pro>
2021-03-01 17:15:59 +0100

    [fix] Better installation scripts for Android (SDK, Cli)

--- scripts/release-android.sh
+++ scripts/release-android.sh
@@ -9,64 +9,34 @@ fi;
 
 # Preparing Android environment
 . ${PROJECT_DIR}/scripts/env-android.sh
-if [[ $? -ne 0 ]]; then
-  exit 1
-fi
+[[ $? -ne 0 ]] && exit 1
+
+ANDROID_OUTPUT_APK_RELEASE=${PROJECT_DIR}/platforms/android/app/build/outputs/apk/release
+APK_UNSIGNED_FILE=${ANDROID_OUTPUT_APK_RELEASE}/${ANDROID_OUTPUT_APK_PREFIX}-release-unsigned.apk
+APK_SIGNED_FILE=${ANDROID_OUTPUT_APK_RELEASE}/${ANDROID_OUTPUT_APK_PREFIX}-release-signed.apk
+
+
+# Remove existing APK
+if [[ -d "${ANDROID_OUTPUT_APK_RELEASE}" ]]; then
+  rm ${ANDROID_OUTPUT_APK_RELEASE}/*.apk
+fi;
 
 cd ${PROJECT_DIR}
 
 # Run the build
 echo "Running cordova build..."
-
 ionic cordova build android --warning-mode=none --color --prod --release
-# TODO: try this :
-#ionic cordova build android --warning-mode=none --color --prod --release --optimizejs --minifyjs --minifycss
+[[ $? -ne 0 ]] && exit 1
 
-if [[ $? -ne 0 ]]; then
-  exit 1
+
+# Sign the APK if possible
+if [[ ! -f "${APK_SIGNED_FILE}" ]]; then
+  . ${PROJECT_DIR}/scripts/release-android-sign.sh
+  [[ $? -ne 0 ]] && exit 1
 fi
 
-# Signature
-KEYSTORE_FILE=${PROJECT_DIR}/.local/Cesium.keystore
-KEY_ALIAS=Cesium
-KEY_PWD=
-APK_DIR=${PROJECT_DIR}/platforms/android/build/outputs/apk/release
-APK_UNSIGNED_FILE=${APK_DIR}/android-release.apk
-BUILD_TOOLS_DIR="${ANDROID_SDK_ROOT}/build-tools/28.*/"
-
-if [[ ! -f "${APK_UNSIGNED_FILE}" ]]; then
-  echo "APK file not found at: ${APK_UNSIGNED_FILE}"
+# Check signed APK exists
+if [[ ! -f "${APK_SIGNED_FILE}" ]]; then
+  echo "No APK file generated!"
   exit 1
-fi
-
-# Check if signed
-cd ${BUILD_TOOLS_DIR}
-./apksigner verify ${APK_UNSIGNED_FILE}
-
-# Not signed ? Do it !
-if [[ $? -ne 0 ]]; then
-  echo "It seems that the APK file ${APK_UNSIGNED_FILE} is not signed !"
-  if [[ ! -f "${KEYSTORE_FILE}" ]]; then
-    echo "ERROR: Unable to sign: no keystore file found at ${KEYSTORE_FILE} !"
-    exit 1
-  fi
-
-  echo "Signing APK file ${APK_UNSIGNED_FILE}..."
-  APK_SIGNED_FILE=${APK_DIR}/android-release-signed.apk
-
-  jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ${KEYSTORE_FILE} ${APK_UNSIGNED_FILE} Cesium
-
-  BUILD_TOOLS_DIR="${ANDROID_SDK_ROOT}/build-tools/28.*/"
-  cd ${BUILD_TOOLS_DIR}
-  ./zipalign -v 4 ${APK_UNSIGNED_FILE} ${APK_SIGNED_FILE}
-
-  ./apksigner verify ${APK_SIGNED_FILE}
-  if [[ $? -ne 0 ]]; then
-    echo "Signing failed !"
-    exit 1
-  fi
-
-  # Do file replacement
-  rm ${APK_UNSIGNED_FILE}
-  mv ${APK_SIGNED_FILE} ${APK_UNSIGNED_FILE}
 fi
